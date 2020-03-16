bf29488def3a301a2b29edecdfd9108281b7cb8a
SanctusAnimus <mijuvo@gmail.com>
2020-03-16 13:22:49 +0200

    fixed pause consuming pick lock
    Also added different text for High Supporters time

--- content/panorama/layout/custom_game/common/pick_locker/pick_locker.js
+++ content/panorama/layout/custom_game/common/pick_locker/pick_locker.js
@@ -4,19 +4,43 @@ var wait_time = [
 	0 // 2 level patreon
 ]
 
+var localized_text = [
+	$.Localize("#HighSupportersOnly"),
+	$.Localize("#SupportersOnly"),
+	$.Localize("#DOTA_Hero_Selection_LOCKIN"),
+]
+
+var interval = 0.3
+
+function _InvokeUpdate(new_time, button) {
+	$.Schedule(interval, function() {
+		_UpdatePickButton(new_time, button)
+	})
+}
+
 function _UpdatePickButton(time, button) {
-	button.GetChild(0).text = $.Localize("#SupportersOnly") + " (" + time + ")"
-	if (time <= 0) {
-		button.SetAcceptsFocus(true)
-		button.BAcceptsInput(true)
-		button.style.saturation = 1
-		button.style.brightness = 1
-		button.GetChild(0).text = $.Localize("#DOTA_Hero_Selection_LOCKIN")
+	// waiting until ban phase or pause expires
+	if (Game.IsInBanPhase() || Game.IsGamePaused()) { 
+		_InvokeUpdate(time, button)
 		return
 	}
-	$.Schedule(1, function() {
-		_UpdatePickButton(time-1, button)
-	})
+
+	let lock_text = localized_text[0]
+	if (time <= 3) {
+		lock_text = localized_text[1]
+	}
+	
+	button.GetChild(0).text = `${lock_text} (${time.toFixed(0)})`
+	if (time < interval) {
+		button.SetAcceptsFocus(true)
+		button.BAcceptsInput(true)
+		button.style.saturation = null
+		button.style.brightness = null
+
+		button.GetChild(0).text = localized_text[2]
+		return
+	}
+	_InvokeUpdate(time - interval, button)
 }
 
 function _InitPickLocker(level) {
@@ -29,8 +53,13 @@ function _InitPickLocker(level) {
 		pick_button.BAcceptsInput(false)
 		pick_button.style.saturation = 0.0
 		pick_button.style.brightness = 0.2
-		pick_button.GetChild(0).style.textTransform = "lowercase"
-		pick_button.GetChild(0).text = $.Localize("#SupportersOnly") + " (" + time + ")"
+
+		let label = pick_button.GetChild(0)
+		label.style.width = "95%"
+		label.style.height = "25px"
+		label.style.horizontalAlign = "left"
+		label.style.textOverflow = "shrink"
+
 		_UpdatePickButton(time, pick_button)
 	}
 }
