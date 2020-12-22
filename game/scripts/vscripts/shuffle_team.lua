36de22f1ff4efe326fae58fa51e97e95fc72fe18
AustraliaIsMyCity <50186248+AustraliaIsMyCity@users.noreply.github.com>
2020-12-22 14:08:27 +1100

    rewrote the team sorting algorithm
    Now runs 3 algorithms and checks which one is the best for that situation:
    Sorting type A: add highest remaining party to lower team until out of parties
    Sorting type B: add highest and lowest to first team, second highest/lowest to second team, etc.
    Sorting type C: add highest party to first team and second highest to second team, then add from top end to lower team and bottom end to higher team

--- game/scripts/vscripts/shuffle_team.lua
+++ game/scripts/vscripts/shuffle_team.lua
@@ -18,8 +18,8 @@ function ShuffleTeam:SortInMMR()
 	local players = {}
 	local playersStats = CustomNetTables:GetTableValue("game_state", "player_stats");
 	if not playersStats then return end
-	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_GOODGUYS, 24 )
-	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_BADGUYS, 24)
+	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_GOODGUYS, 12 )
+	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_BADGUYS, 12)
 
 	local phantomPartyID = 456332
 	for playerId = 0, 23 do
@@ -39,8 +39,7 @@ function ShuffleTeam:SortInMMR()
 			mmr = playerRating
 		}
 	end
-
-	local teams = { [2] = { players = {}, mmr = 0}, [3] = { players = {}, mmr = 0}}
+	
 	local partiesMMR = {}
 
 	for playerId, data in pairs(players) do
@@ -57,47 +56,171 @@ function ShuffleTeam:SortInMMR()
 		return a.mmr > b.mmr
 	end)
 
-	local SortTeam = function(MIN_DIFFPlayerCount)
-		for _, partyData in pairs(sortedParties) do
-			if #partyData.players >= MIN_DIFFPlayerCount and not partyData.sorted then
-				partyData.sorted = true
-				local teamId = 2
-				if teams[teamId].mmr > teams[3].mmr then
-					teamId = 3
+	-- This is just a table deepcopy function I found on stackexchange, feel free to replace it with whatever equivelant 12v12 has or move it to a utility functions file
+	local function deepcopy(orig, copies)
+		copies = copies or {}
+		local orig_type = type(orig)
+		local copy
+		if orig_type == 'table' then
+			if copies[orig] then
+				copy = copies[orig]
+			else
+				copy = {}
+				copies[orig] = copy
+				for orig_key, orig_value in next, orig, nil do
+					copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
 				end
-				for _, playerId in pairs(partyData.players) do
-					if #teams[teamId].players >= MAX_PLAYERS_IN_TEAM then
-						teamId = teamId == 2 and 3 or 2
+				setmetatable(copy, deepcopy(getmetatable(orig), copies))
+			end
+		else -- number, string, boolean, etc
+			copy = orig
+		end
+		return copy
+	end
+	
+	local SortTeams = function()
+	
+		local teams = {
+			[2] = {
+				players = {},
+				mmr = 0
+			}, 
+			[3] = {
+				players = {},
+				mmr = 0
+			}
+		}
+	
+		local teamsA = deepcopy(teams)
+		local teamsB = deepcopy(teams)
+		local teamsC = deepcopy(teams)
+		
+		-- Sorting type A: add highest remaining party to lower team until out of parties
+		
+		for _, partyData in pairs(sortedParties) do -- For each party in the party list
+			local teamId = teamsA[2].mmr >= teamsA[3].mmr and 3 or 2 -- Select team with lowest mmr
+			
+			-- Add highest mmr party to selected team
+			for _, playerId in pairs(partyData.players) do -- For each player in highest mmr party
+				-- This will split up the final party in very few scenarios
+				-- where the mmr values are very unbalanced and the lowest
+				-- mmr party has less than half the mmr of any solo player
+				if #teamsA[teamId].players == MAX_PLAYERS_IN_TEAM then -- If selected team is full
+					teamId = teamId == 2 and 3 or 2 -- Select other team
+				end
+				
+				table.insert(teamsA[teamId].players, playerId) -- Add player to team
+				teamsA[teamId].mmr = teamsA[teamId].mmr + players[playerId].mmr -- Add players mmr to team
+			end
+		end
+			
+		--======================================================================================--
+			
+		-- Sorting type B: add highest and lowest to first team, second highest/lowest to second team, repeat until out of parties
+		
+		for partyID = 1, #sortedParties do -- For each partyID (used instead of pairs() because I grab some parties from the lower end)
+			local teamId = teamsB[2].mmr >= teamsB[3].mmr and 3 or 2 -- Select team with lowest mmr
+			
+			-- Add highest mmr party to selected team
+			if partyID <= math.ceil(#sortedParties/2) then
+				for _, playerId in pairs(sortedParties[partyID].players) do -- For each player in highest party
+					if #teamsB[teamId].players == MAX_PLAYERS_IN_TEAM then -- If selected team is full
+						teamId = teamId == 2 and 3 or 2 -- Select other team
 					end
-					table.insert(teams[teamId].players, playerId)
-					teams[teamId].mmr = (teams[teamId].mmr or 0) + players[playerId].mmr
-					local player = PlayerResource:GetPlayer(playerId)
-					if player then
-						player:SetTeam(teamId)
-						PlayerResource:SetCustomTeamAssignment(playerId,teamId)
+					table.insert(teamsB[teamId].players, playerId) -- Add player to team
+					teamsB[teamId].mmr = teamsB[teamId].mmr + players[playerId].mmr -- Add players mmr to team
+				end
+			end
+			-- Add lowest mmr party to selected team
+			if partyID < math.ceil(#sortedParties/2) then
+				for _, playerId in pairs(sortedParties[#sortedParties - partyID + 1].players) do -- For each player in highest party
+					if #teamsB[teamId].players == MAX_PLAYERS_IN_TEAM then -- If selected team is full
+						teamId = teamId == 2 and 3 or 2 -- Select other team
 					end
+					table.insert(teamsB[teamId].players, playerId) -- Add player to team
+					teamsB[teamId].mmr = teamsB[teamId].mmr + players[playerId].mmr -- Add players mmr to team
 				end
 			end
 		end
+		
+		--======================================================================================--
+		
+		-- Sorting type C: add highest party to each team then add from top end to lower team and bottom end to higher team
+		
+		-- Add highest mmr party to team 1
+		for _, playerId in pairs(sortedParties[1].players) do -- For each player in highest party
+			table.insert(teamsC[2].players, playerId) -- Add player to party
+			teamsC[2].mmr = teamsC[2].mmr + players[playerId].mmr -- Add players mmr to party
+		end
+		
+		-- Add second highest mmr party to team 2
+		for _, playerId in pairs(sortedParties[2].players) do -- For each player in highest party
+			table.insert(teamsC[3].players, playerId) -- Add player to party
+			teamsC[3].mmr = teamsC[3].mmr + players[playerId].mmr -- Add players mmr to party
+		end
+		
+		for partyID = 3, math.ceil(#sortedParties/2+1) do -- for each of half the remaining partyID's (used instead of pairs() because I grab some parties from the lower end)
+			local teamId = teamsC[2].mmr >= teamsC[3].mmr and 3 or 2 -- Select lowest team
+			
+			-- Add highest mmr party to lower team
+			for _, playerId in pairs(sortedParties[partyID].players) do -- For each player in highest party
+				if #teamsC[teamId].players == MAX_PLAYERS_IN_TEAM then -- If selected team is full
+					teamId = teamId == 2 and 3 or 2 -- Select other team
+				end
+				table.insert(teamsC[teamId].players, playerId) -- Add player to party
+				teamsC[teamId].mmr = teamsC[teamId].mmr + players[playerId].mmr -- Add players mmr to party
+			end
+			
+			teamId = teamId == 2 and 3 or 2 -- Select other team
+			
+			-- Add lowest mmr party to higher team
+			if partyID < math.ceil(#sortedParties/2+1) then
+				for _, playerId in pairs(sortedParties[#sortedParties - partyID + 3].players) do -- For each player in highest party
+					if #teamsC[teamId].players == MAX_PLAYERS_IN_TEAM then -- If selected team is full
+						teamId = teamId == 2 and 3 or 2 -- Select other team
+					end
+					table.insert(teamsC[teamId].players, playerId) -- Add player to party
+					teamsC[teamId].mmr = teamsC[teamId].mmr + players[playerId].mmr -- Add players mmr to party
+				end
+			end
+		end
+		
+		--======================================================================================--
+		
+		-- Find the difference in mmr for each algorithm
+		mmrDiffA = math.floor(math.abs(teamsA[2].mmr - teamsA[3].mmr) / MAX_PLAYERS_IN_TEAM)
+		mmrDiffB = math.floor(math.abs(teamsB[2].mmr - teamsB[3].mmr) / MAX_PLAYERS_IN_TEAM)
+		mmrDiffC = math.floor(math.abs(teamsC[2].mmr - teamsC[3].mmr) / MAX_PLAYERS_IN_TEAM)
+		
+		-- Return the algorithm with the lowest MMR difference
+		return (mmrDiffA < mmrDiffB and mmrDiffA < mmrDiffC) and teamsA or mmrDiffB < mmrDiffC and teamsB or teamsC
 	end
-	SortTeam(2)
-	SortTeam(1)
-	self.weakTeam = teams[2].mmr < teams[3].mmr and 2 or 3
-	self.mmrDiff = math.abs(math.floor(teams[2].mmr/MAX_PLAYERS_IN_TEAM) - math.floor(teams[3].mmr/MAX_PLAYERS_IN_TEAM))
+	
+	local sortedTeams = SortTeams()
+	
+	self.weakTeam = sortedTeams[2].mmr < sortedTeams[3].mmr and 2 or 3
+	self.mmrDiff = math.floor(math.abs(sortedTeams[2].mmr - sortedTeams[3].mmr) / MAX_PLAYERS_IN_TEAM)
 
 	--DEBUG PRINT PART
-	for teamId,teamData in pairs(teams) do
+	for teamId,teamData in pairs(sortedTeams) do
 		AutoTeam:Debug("")
 		AutoTeam:Debug("Team: ["..teamId.."]")
 		for id, playerId in pairs(teamData.players) do
+			
+			-- Add each player to their team
+			local player = PlayerResource:GetPlayer(playerId)
+			if player then
+				player:SetTeam(teamId)
+				PlayerResource:SetCustomTeamAssignment(playerId,teamId)
+			end
+		
 			AutoTeam:Debug(id .. " pid: "..playerId .. "	> "..playerId.." MMR: "..players[playerId].mmr .. " TEAM: "..players[playerId].partyID)
 		end
 	end
+	
 	AutoTeam:Debug("")
 	AutoTeam:Debug("Team 2 averages MMR: " .. math.floor(teams[2].mmr/MAX_PLAYERS_IN_TEAM))
-	AutoTeam:Debug("Team 3 averages MMR: " .. math.floor(teams[3].mmr/MAX_PLAYERS_IN_TEAM))
-	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_GOODGUYS, 12)
-	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_BADGUYS,12)
+	AutoTeam:Debug("Team 3 averages MMR: " .. math.floor(teams[3].mmr/MAX_PLAYERS_IN_TEAM))	
 end
 
 function ShuffleTeam:SendNotificationForWeakTeam()
